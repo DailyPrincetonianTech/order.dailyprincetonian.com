3570683e06b3acc39151da6f659943d9d8f86a5e
Pranav Avva <pranav.avva@gmail.com>
2021-08-01 16:10:04 -0400

    trying to fix this font issue

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
+import './css/fonts.css';
+import './css/index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
