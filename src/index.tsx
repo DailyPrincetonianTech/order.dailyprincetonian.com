bb97732f5a45fcecc46fd79ea6ee0b62ecbc67a7
Pranav Avva <pranav.avva@gmail.com>
2021-08-01 16:14:53 -0400

    font issue part 13

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
-import './css/fonts.css';
 import './css/index.css';
 import reportWebVitals from './reportWebVitals';
 
