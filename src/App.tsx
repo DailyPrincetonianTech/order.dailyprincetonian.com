8d88f11413c75ecbe6d9a72bd045fe63f5ae39a1
Pranav Avva <pranav.avva@gmail.com>
2021-07-31 22:27:01 -0400

    router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Navbar from './components/navbar/Navbar';
+import {
+    HashRouter as Router,
+    Route,
+    Switch
+} from 'react-router-dom';
 
 function App() {
-  return (
-    <div>
-      <Navbar/>
-      Hello world!
-    </div>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route exact path="/" component={() => <h1>Home</h1>} />
+                <Route path="/gear" component={() => <h1>Gear</h1>} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
