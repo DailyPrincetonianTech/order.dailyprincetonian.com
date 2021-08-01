6f93ff4873ac367a46ca505da0694cec6700169b
Pranav Avva <pranav.avva@gmail.com>
2021-08-01 17:28:05 -0400

    extra button

--- src/components/gear/GearItem.tsx
+++ src/components/gear/GearItem.tsx
@@ -7,19 +7,28 @@ interface GearItemProps {
     image: string;
     price: number;
     shipping: number;
+    description?: string;
+    buttonText?: string;
+    buttonHref?: string;
 }
 
 export default function GearItem(props: GearItemProps) {
-    const { name, slug, image, price, shipping } = props;
+    const { name, slug, image, price, shipping, description, buttonText, buttonHref } = props;
     return (
         <div>
             <img src={image} alt={name} className="img-fluid" style={{ maxHeight: 600 }} />
             <p>{name}</p>
             <p>${price} + ${shipping} shipping</p>
+            <p>{description}</p>
             <div className="d-grid col-7 col-md-3 mx-auto">
                 <button type="button" className="btn btn-dark text-uppercase" data-bs-toggle="modal" data-bs-target={`#${slug}Modal`}>
                     Order Now
                 </button>
+                {buttonText !== "" &&
+                    <a type="button" className="btn btn-primary text-uppercase text-nowrap" href={buttonHref}>
+                        {buttonText}
+                    </a>
+                }
             </div>
             <div className="modal fade" id={`${slug}Modal`} aria-labelledby="modalLabel" aria-hidden="true">
                 <div className="modal-dialog">
