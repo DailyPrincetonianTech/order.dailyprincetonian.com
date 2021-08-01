8988e23588274e7c8f0a49d4a6d7119872142b9d
Pranav Avva <pranav.avva@gmail.com>
2021-08-01 15:25:49 -0400

    style touchups

--- src/components/gear/GearItem.tsx
+++ src/components/gear/GearItem.tsx
@@ -12,16 +12,14 @@ interface GearItemProps {
 export default function GearItem(props: GearItemProps) {
     const { name, slug, image, price, shipping } = props;
     return (
-        <>
-            <div>
-                <img src={image} alt={name} style={{ maxHeight: 600 }} />
-                <p>{name}</p>
-                <p>${price} + ${shipping} shipping</p>
-                <div className="d-grid col-4 mx-auto">
-                    <button type="button" className="btn btn-dark text-uppercase" data-bs-toggle="modal" data-bs-target={`#${slug}Modal`}>
-                        Order Now
-                    </button>
-                </div>
+        <div>
+            <img src={image} alt={name} className="img-fluid" style={{ maxHeight: 600 }} />
+            <p>{name}</p>
+            <p>${price} + ${shipping} shipping</p>
+            <div className="d-grid col-7 col-md-3 mx-auto">
+                <button type="button" className="btn btn-dark text-uppercase" data-bs-toggle="modal" data-bs-target={`#${slug}Modal`}>
+                    Order Now
+                </button>
             </div>
             <div className="modal fade" id={`${slug}Modal`} aria-labelledby="modalLabel" aria-hidden="true">
                 <div className="modal-dialog">
@@ -31,21 +29,23 @@ export default function GearItem(props: GearItemProps) {
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
                         </div>
                         <div className="modal-body" id={`${slug}ModalBody`}>
-                            <p>{name}: ${price + shipping} USD</p>
+                            <p>${price + shipping} USD</p>
                             <PayPalButton
                                 amount={price + shipping}
                                 options={{
                                     clientId: "AeiJZG5XN-aJrlY7i94_IPWTQkzNwQze_KPb-5uNuvp4Lrh4SwAn4BwqYGFOmPg7TTN8NXf-K51_Qwqc",
                                     currency: "USD",
-                                }}/>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
+                                }}
+                                style={{
+                                    shape: 'rect',
+                                    color: 'silver',
+                                    layout: 'vertical',
+                                    label: 'paypal',
+                                }} />
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
