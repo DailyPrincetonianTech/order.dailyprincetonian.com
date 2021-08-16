5afa67eebb79e5a3109f21a084823b28e0dd4887
Anika <agarwal.anika@yahoo.com>
2021-08-16 08:36:47 +0800

    close the crewneck and bag sale (button change)

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -51,7 +51,7 @@ export default function Main() {
                         At the time, a copy of the print paper cost three cents.
                     </p>
                     <div className="d-grid col-8 col-md-4 mx-auto">
-                        <Link to="/gear" className="btn btn-dark text-uppercase fs-5">Order Now</Link>
+                        <button className="btn btn-secondary disabled text-uppercase fs-5">Closed</button>
                     </div>
                 </div>
             </div>
