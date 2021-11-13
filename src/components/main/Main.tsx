8aa2dc91ba702e6146c9659e7b58f1405f932244
Roma Bhattacharjee <roma0615@gmail.com>
2021-11-13 12:58:30 -0500

    Add framed bonfire interest form

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -9,6 +9,37 @@ export default function Main() {
                 <div className="col-12 col-md-6">
                     <div className="row d-flex align-items-center">
                         <div className="col-12">
+                            <img src="https://assets.dailyprincetonian.com/order.dailyprincetonian.com/framed_front_page.jpg"
+                                alt="Blair Poster" className="img-fluid" width="75%" />
+                        </div>
+                    </div>
+                </div>
+                <div className="col-12 col-md-6 align-self-center order-first order-md-1">
+                    <h4 className={styles.textOrange + " text-center text-uppercase fw-bold"}>
+                        Framed Princeton Football Bonfire Daily Princetonian Interest Form
+                    </h4>
+                    <p className="fs-6">
+                        Celebrate Princeton Football’s victory over Harvard and Yale this season with a framed front page of the Daily Princetonian featuring the bonfire and Princeton Football Team! In keeping with ‘Prince’ tradition, we are excited to offer this special piece of history to Princeton Football players, staff, fans, and alumni.  Please see a sample on the left (this is NOT the final/official product)
+                    </p>
+                    <p className="fs-6">
+                        Sign up here to receive more information!
+                    </p>
+                    <p className="fs-6">
+                        The framed front page will display the Daily Princetonian’s coverage of bonfire and likely cost between $75-100 to cover printing and framing expenses.
+                    </p>
+                    <p className="fs-6">
+                        We plan to distribute the frames around the end of November/early December (making this the perfect the Holiday gift too!).
+                        Please email <a href="mailto:john@dailyprincetonian.com">john@dailyprincetonian.com</a> if you have any questions or concerns.
+                    </p>
+                    <div className="d-grid col-8 col-md-4 mx-auto pb-3 pb-md-auto">
+                        <a href="https://forms.gle/PvY6mtehvheN73Y6A" className="btn btn-dark text-uppercase fs-5">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+            <div className="row pb-3">
+                <div className="col-12 col-md-6">
+                    <div className="row d-flex align-items-center">
+                        <div className="col-12">
                             <img src="https://assets.dailyprincetonian.com/order.dailyprincetonian.com/blair-poster.png"
                                 alt="Blair Poster" className="img-fluid" width="75%" />
                         </div>
